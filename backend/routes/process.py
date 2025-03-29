fc8e708f49ebe7d2942d91156eca0378c51c0b1c
Siam Salam <82978084+siam-s@users.noreply.github.com>
2025-03-29 19:39:35 -0400

    updated process.py and transcription.py, untested as of right

--- backend/routes/process.py
+++ backend/routes/process.py
@@ -1,19 +1,21 @@
 import os
 import ffmpeg
 from flask import session
+from ..services import transcription, slides
 
 filename = session.get("filename")
 file_type = session.get("file_type")
+output_file = "lecture_to_audio.mp3"
 match file_type:
     case "mp4":
-        output_file = "lecture_to_audio.mp3"
         ffmpeg.input(filename).output(output_file, format="mp3", audio_bitrate="128k").run()
+        print(transcription.transcribe_audio(filename))
     case "mov":
-        output_file = "lecture_to_audio.mp3"
         ffmpeg.input(filename).output(output_file, format="mp3", audio_bitrate="128k").run()
+        print(transcription.transcribe_audio(filename))
     case "mp3":
-        output_file = "lecture_to_audio.mp3"
         os.rename(filename, output_file)
+        print(transcription.transcribe_audio(filename))
     # case "pptx":
 
     # case "pdf":
