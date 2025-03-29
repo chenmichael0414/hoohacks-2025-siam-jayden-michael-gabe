d70ac8b7265dc55086755ccd1f02ae5d4d9a0d88
Siam Salam <82978084+siam-s@users.noreply.github.com>
2025-03-29 19:39:35 -0400

    updated process.py and transcription.py, untested as of right

--- backend/services/transcription.py
+++ backend/services/transcription.py
@@ -1,13 +1,13 @@
 import os
 from google import genai
 from dotenv import load_dotenv
-from ..routes import process
 
 load_dotenv()
 api_key = os.getenv('google_API_key')
 client = genai.Client(api_key=api_key)
 
 client.model = "gemini-2.0-flash"
+
 
 def transcribe_audio(file_path):
     prompt = "Generate a transcript of only the speech in the video. Format it well."
