c0c05d858726da8cac0aa690ace62a1c9be51643
Siam Salam <82978084+siam-s@users.noreply.github.com>
2025-03-30 05:16:01 -0400

    hopefully working pptx.py, will edit if needed

--- backend/services/pptx.py
+++ backend/services/pptx.py
@@ -3,6 +3,7 @@ from google import genai
 from dotenv import load_dotenv
 from pptx import Presentation
 from PIL import Image
+import pytesseract
 import io
 
 load_dotenv()
@@ -11,20 +12,21 @@ client = genai.Client(api_key=api_key)
 
 
 def detect_and_write_slides_from_pptx(file_path):
+    text = ""
     prs = Presentation(file_path)
-    slide_images = []
     for slide in prs.slides:
         for i, shape in slide.shapes:
             if hasattr(shape, "image"):
                 img_data = slide.shapes[0].image.blob
                 img = Image.open(io.BytesIO(img_data))
+                text += pytesseract.image_to_string(img) + "\n"
     prompt = (
-        "Generate well-formatted and clean notes with titles and headers from the images that you are supplied with."
+        "Generate well-formatted and clean notes with titles and headers from the text that you are supplied with."
         "There may be mistakes in formatting and spelling as of right. Please correct them, if necessary."
-        "Provide extensive timestamps in MM:SS format for each section of notes that you generate.")
+        "If there is no text provided, do not generate a response. Or instead, generate an empty response.")
 
     response = client.models.generate_content(
         model='gemini-2.0-flash',
-        contents=[prompt, slide_images],
+        contents=[prompt, text]
     )
     return response.text
