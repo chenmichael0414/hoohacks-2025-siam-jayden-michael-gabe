17d91edae5b0fc404cf73f2d33754d2d0272ebc1
Siam Salam <82978084+siam-s@users.noreply.github.com>
2025-03-30 02:55:32 -0400

    Faulty transcription and or process for Michael

--- backend/routes/process.py
+++ backend/routes/process.py
@@ -5,27 +5,32 @@ from services import transcription, slides
 
 process_bp = Blueprint("process", __name__)
 
+
 @process_bp.route("/processing")
 def processing_page():
     filename = session.get("filename")
     if not filename:
         return "No file uploaded", 400
     file_type = session.get("file_type")
-    file_path = session.get("file_path")
+    input_file_path = session.get("file_path")
+    print(input_file_path)
     output_file_path = os.path.join(current_app.config["UPLOAD_FOLDER"], "lecture_to_audio.mp3")
-    # Emit progress: Audio extraction starts
-    
-    match file_type:
-        case "mp4" | "mov":
-            ffmpeg.input(filename).output(output_file_path, format="mp3", audio_bitrate="128k").run()
-        case "mp3":
-            os.rename(file_path, output_file_path)  
-    print(file_path)
+
+    if os.path.exists(output_file_path):
+        match file_type:
+            case "mp4" | "mov":
+                ffmpeg.input(filename).output(input_file_path, format="mp3", audio_bitrate="128k").run()
+            case "mp3":
+                print(output_file_path)
+                os.replace(input_file_path, output_file_path)
+    else:
+        match file_type:
+            case "mp4" | "mov":
+                ffmpeg.input(filename).output(output_file_path, format="mp3", audio_bitrate="128k").run()
+            case "mp3":
+                os.rename(input_file_path, output_file_path)
     print(transcription.transcribe_audio(output_file_path))
-    
 
     return render_template("processing.html")
-
-    
 
     # case "pdf":
