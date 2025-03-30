8f275275146f671bc7d4632ad68c7e0e00fe55e0
Siam Salam <82978084+siam-s@users.noreply.github.com>
2025-03-30 01:27:48 -0400

    add __init__.py to all modules necessary to make site work again

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, redirect, session, url_for, request
-from ..services.auth_config import auth0
+from services.auth_config import auth0
 import os
 
 auth_bp = Blueprint("auth", __name__)
 
+
 @auth_bp.route("/login")
 def login():
     return auth0.authorize_redirect(redirect_uri=os.getenv("AUTH0_CALLBACK_URL"))
+
 
 @auth_bp.route("/callback")
 def callback():
@@ -18,6 +20,7 @@ def callback():
         "email": user_info["email"],
     }
     return redirect(url_for("index"))  # or wherever you want
+
 
 @auth_bp.route("/logout")
 def logout():
