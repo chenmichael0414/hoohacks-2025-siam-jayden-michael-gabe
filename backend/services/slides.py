d22a420f3ebcb691566f17fd1fba5e67dbdc0f83
Siam Salam <82978084+siam-s@users.noreply.github.com>
2025-03-29 23:08:44 -0400

    Pushing hopefully working slides.py, as well as updateed requirements.txt

--- backend/services/slides.py
+++ backend/services/slides.py
@@ -1,18 +1,26 @@
 import os
 from google import genai
 from dotenv import load_dotenv
+import pytesseract
+from pdf2image import convert_from_path
 
 load_dotenv()
 api_key = os.getenv('google_API_key')
 client = genai.Client(api_key=api_key)
 
+prompt = "Generate well-formatted and clean notes with titles and headers from the text that you are supplied with." + \
+         "There may be mistakes in formatting and spelling as of right. Please correct them, if necessary."
 
-def generate_notes_from_slides(slides):
-    generated_notes = []
-    for slide in slides:
-        response = client.models.generate_content(
-            model="gemini-2.0-flash",
-            contents=slide
-        )
-        generated_notes.append(response)
+
+def parse_generate_pdf(pdf_path):
+    images = convert_from_path(pdf_path)
+    text = ""
+    for image in images:
+        text += pytesseract.image_to_string(image)
+    generated_notes = client.models.generate_content(
+        model='gemini-2.0-flash',
+        contents=[prompt, text]
+    )
     return generated_notes
+# make sure this returns formatted text, be it JSON or other formatNOT a string,
+# unless there are no other options, or it actually formats it well
